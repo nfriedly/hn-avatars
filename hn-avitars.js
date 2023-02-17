9007193bad5bb290e94e536be72e3bb56c91945a
Nathan Friedly <nathan@nfriedly.com>
2023-02-17 10:17:19 -0500

    Initial code by tomxor
    https://news.ycombinator.com/item?id=30668137

---
+++ hn-avitars.js
@@ -0,0 +1,1 @@
+for(u of document.querySelectorAll('.hnuser'))for(u.prepend(c=document.createElement('canvas')),x=c.getContext('2d'),c.width=18,c.height=14,s=u.innerText,r=1,i=28+s.length;i--;i<28?r>>>29>X*X/3+Y/2&&x.fillRect(6+2*X,2*Y,2,2)&x.fillRect(6-2*X,2*Y,2,2):r+=s.charCodeAt(i-28,x.fillStyle='#'+(r>>8&0xFFFFFF).toString(16)))r^=r<<13,r^=r>>>17,r^=r<<5,X=i&3,Y=i>>2
